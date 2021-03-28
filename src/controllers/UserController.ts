4d5957a0d1567b86a8f7c7f1b181894651cbc1d7
Patrick Mazzuco <patrick.flores@acad.pucrs.br>
2021-03-28 16:40:57 -0300

    Add example files and example user creation flow

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+
+import { UserService } from "../services";
+
+const createUserExample = async (req: Request, res: Response) => {
+  const { email } = req.body;
+
+  try {
+    const response = await UserService.createUserExample({
+      email,
+    });
+
+    return res.status(201).json(response);
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .json({ message: "Internal server error", code: err.code });
+  }
+};
+
+export default { createUserExample };
---
+++ src/repositories/UserRepository.ts
@@ -0,0 +1,14 @@
+import { EntityRepository, Repository } from "typeorm";
+
+import { User } from "../models/User";
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  findByEmail(email: string) {
+    return this.findOne({
+      where: {
+        email,
+      },
+    });
+  }
+}
