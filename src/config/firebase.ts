aa47b311e18642da454146fed8320189bdd42c71
johnnyloreano <laureano.pedrojoao@gmail.com>
2022-07-27 11:31:02 -0300

    Adding ENV for firebase config

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,5 +1,11 @@
 import admin from "firebase-admin";
 
-admin.initializeApp({ credential: admin.credential.applicationDefault() });
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
+});
 
 export default admin;
