537b3681aba241e3dc60168a89a119137e6909b4
johnnyloreano <laureano.pedrojoao@gmail.com>
2022-07-27 10:01:08 -0300

    fix on lower case column name

--- src/config/swaggerConfig.js
+++ src/config/swaggerConfig.js
@@ -43,21 +43,59 @@ var __importStar =
     __setModuleDefault(result, mod);
     return result;
   };
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
+    return new (P || (P = Promise))(function (resolve, reject) {
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+  };
 var __importDefault =
   (this && this.__importDefault) ||
   function (mod) {
     return mod && mod.__esModule ? mod : { default: mod };
   };
 Object.defineProperty(exports, "__esModule", { value: true });
-const swaggerDoc = __importStar(require("../../dist/swagger.json"));
 const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
 const express_1 = __importDefault(require("express"));
 const routes_1 = require("../routes/routes");
 const swaggerServer = (0, express_1.default)();
 (0, routes_1.RegisterRoutes)(swaggerServer);
-swaggerServer.use(
-  "/doc",
-  swagger_ui_express_1.default.serve,
-  swagger_ui_express_1.default.setup(swaggerDoc)
+swaggerServer.use("/doc", swagger_ui_express_1.default.serve, (_req, res) =>
+  __awaiter(void 0, void 0, void 0, function* () {
+    return res.send(
+      swagger_ui_express_1.default.generateHTML(
+        yield Promise.resolve().then(() =>
+          __importStar(require("../../dist/swagger.json"))
+        )
+      )
+    );
+  })
 );
 exports.default = swaggerServer;
--- src/config/swaggerConfig.ts
+++ src/config/swaggerConfig.ts
@@ -1,4 +1,4 @@
-import * as swaggerDoc from "../../dist/swagger.json";
+import { Response, Request } from "express";
 import swaggerUI from "swagger-ui-express";
 import express from "express";
 import { RegisterRoutes } from "../routes/routes";
@@ -7,6 +7,14 @@ const swaggerServer = express();
 
 RegisterRoutes(swaggerServer);
 
-swaggerServer.use("/doc", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
+swaggerServer.use(
+  "/doc",
+  swaggerUI.serve,
+  async (_req: Request, res: Response) => {
+    return res.send(
+      swaggerUI.generateHTML(await import("../../dist/swagger.json"))
+    );
+  }
+);
 
 export default swaggerServer;
